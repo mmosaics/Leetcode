579abeee6afa094c92943d309df17b8bcd92047e
hlr <755593119@qq.com>
2020-08-15 00:25:25 +0800

    update

--- solution/dfs/[130]被围绕的区域.cpp
+++ solution/dfs/[130]被围绕的区域.cpp
@@ -32,6 +32,7 @@ public:
     void solve(vector<vector<char>>& board) {
         if(board.size() == 0)
             return;
+        
         int rowSize = board.size();
         int columnSize = board[0].size();
         //先对边缘为O的进行深度优先搜索
